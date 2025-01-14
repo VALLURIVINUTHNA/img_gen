fe5315050d29df5cc990abc93854c998a60fb5e4
VALLURIVINUTHNA <126446412+VALLURIVINUTHNA@users.noreply.github.com>
2025-01-14 20:32:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,6 @@ def generate_image():
         print("Error generating image:", e)
         return jsonify({"error": str(e)}), 500
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000)) 
     app.run(host='0.0.0.0', port=port, debug=True)
